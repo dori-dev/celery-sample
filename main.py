7a1817fd87c86444e8d3cd5f7682baf8338ba60e
dori-dev <mr.dori.dev@gmail.com>
2023-04-27 22:21:32 +0330

    using rpc in celery

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from time import sleep
 from celery import Celery
 
 app = Celery(
-    main='add',
+    main='main',
+    backend='rpc://',
     broker='amqp://localhost',
 )
 
 
 @app.task
 def add(x, y):
-    sleep(15)
+    sleep(5)
     return x + y
 
 
 if __name__ == '__main__':
-    add.delay(5, 3)
+    result = add.delay(5, 3)
+    # result.ready()
+    print(result.get())
